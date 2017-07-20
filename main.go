23fe49f3bc0caadca55ece87c06b19b2b3505ce6
bmacdonald <bmacdonald@gogoair.com>
2017-07-20 10:00:35 -0600

    Added REPL

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
-package MonkeyLang
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"./repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		user.Username)
+	repl.Start(os.Stdin, os.Stdout)
+}
--- repl/repl.go
+++ repl/repl.go
@@ -1,1 +1,29 @@
 package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"../lexer"
+	"../token"
+)
+
+const PROMPT = "[%d] >> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	var i = 0
+	for {
+		fmt.Printf(PROMPT, i)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+		i++
+	}
+}
