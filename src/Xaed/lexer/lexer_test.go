48099136116e26d7a96c3ed44ba25f6503d6964b
bmacdonald <bmacdonald@gogoair.com>
2017-07-23 13:31:55 -0600

    Moved files around.

---
+++ src/Xaed/lexer/lexer_test.go
@@ -0,0 +1,149 @@
+package lexer
+
+import (
+	"testing"
+	"Xaed/token"
+)
+func TestNextToken(t *testing.T) {
+	input := `
+let five := 5;
+let ten := 10;
+let add := fn(x, y) {
+	x + y;
+};
+let result := add(five, ten);
+!-/*5;
+
+five: ten;
+five = ten;
+
+5 < 10 > 5;
+if (5 < 10) {
+   return true;
+} else {
+   return false;
+}
+
+10 == 10;
+10 != 9;
+
+five.add := add;
+	`
+
+	/*
+	 >> foobar := (t, t+1)
+	 >> foobar(1)
+	 2
+	 >> Foobar := Object
+	 >> Foobar.spam := foobar
+	 >> Foobar.spam(2)
+	 3
+	 */
+
+	tests := []struct {
+		expectedType token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, ":="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, ":="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, ":="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, ":="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IDENT, "five"},
+		{token.COLON, ":"},
+		{token.IDENT, "ten"},
+		{token.SEMICOLON, ";"},
+		{token.IDENT, "five"},
+		{token.REASSIGN, "="},
+		{token.IDENT, "ten"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
+		{token.IDENT, "five"},
+		{token.SLOT, "."},
+		{token.IDENT, "add"},
+		{token.ASSIGN, ":="},
+		{token.IDENT, "add"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
---
+++ src/Xaed/main.go
@@ -0,0 +1,18 @@
+package Xaed
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"Xaed/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		user.Username)
+	repl.Start(os.Stdin, os.Stdout)
+}
