05bcb15474291b903e9811dfb169be43da01fcd6
bmacdonald <bmacdonald@gogoair.com>
2017-08-13 17:03:34 -0600

    Update repl and Objects.

--- src/Xaed/repl/repl.go
+++ src/Xaed/repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"Xaed/lexer"
 	"Xaed/token"
+	"Xaed/parser"
 )
 
 const PROMPT = "[%d] >> "
@@ -21,9 +22,21 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 		i++
 	}
 }
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
+
